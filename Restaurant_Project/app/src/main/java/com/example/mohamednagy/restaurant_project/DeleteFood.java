3085e2eb289d0d2bdd767a4094db48ad74943ae5
HeshamSalama <eslamsalama2012@gmail.com>
2017-05-15 13:13:57 +0200

    delete food design

---
+++ Restaurant_Project/app/src/main/java/com/example/mohamednagy/restaurant_project/DeleteFood.java
@@ -0,0 +1,22 @@
+package com.example.mohamednagy.restaurant_project;
+
+import android.app.Activity;
+import android.view.View;
+import android.widget.AdapterView;
+
+/**
+ * Created by Hesham Salama on 5/15/2017.
+ */
+
+public class DeleteFood extends Activity implements AdapterView.OnItemSelectedListener {
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
+}
