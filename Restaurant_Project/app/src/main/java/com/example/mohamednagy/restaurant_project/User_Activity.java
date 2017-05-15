64521e19bdeffa506d5c68c708b4bdeb808b847b
mohamednagy018 <mohamednagy018@gmail.com>
2017-05-15 16:22:20 +0200

    action bar list is added

--- Restaurant_Project/app/src/main/java/com/example/mohamednagy/restaurant_project/User_Activity.java
+++ Restaurant_Project/app/src/main/java/com/example/mohamednagy/restaurant_project/User_Activity.java
@@ -1,10 +1,17 @@
 package com.example.mohamednagy.restaurant_project;
 
 import android.app.ActionBar;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 public class User_Activity extends AppCompatActivity {
 
@@ -12,5 +19,45 @@ public class User_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_layout);
+
+        Toolbar userToolbar = (Toolbar) findViewById(R.id.usertoolbar);
+        setSupportActionBar(userToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflate = getMenuInflater();
+        menuInflate.inflate(R.menu.user_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        FragmentManager fm = getFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        if(item.getTitle().equals("Food"))
+        {
+            Food food = new Food();
+            Bundle bundle = getIntent().getExtras();
+            food.setArguments(bundle);
+            ft.replace(R.id.fragmentContent,food);
+            ft.commit();
+        }else if (item.getTitle().equals("Profile"))
+        {
+            Profile prof = new Profile();
+            Bundle bundle = getIntent().getExtras();
+            prof.setArguments(bundle);
+
+            ft.replace(R.id.fragmentContent,prof);
+            ft.commit();
+        }else if (item.getTitle().equals("About Us"))
+        {
+            About about = new About();
+            Bundle bundle = getIntent().getExtras();
+            about.setArguments(bundle);
+            ft.replace(R.id.fragmentContent,about);
+            ft.commit();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
